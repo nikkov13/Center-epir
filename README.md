703c90632b99fac074fbc1fce91d72ba84e0bb98
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-26 17:45:33 +0000

    Deploy nikkov13/Center-epir to github.com/nikkov13/Center-epir.git:gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# Landing for Center EPIR
-You can see demo-version of this site [there](https://nikkov13.github.io/Center-epir/build/index.html)
--- source/js/scroll.js
+++ source/js/scroll.js
@@ -1,12 +1,17 @@
 "use strict";
 
+
 $(document).ready(function() {
+  let $window = $(window);
   
   //Плавные переходы по якорям
   $(`.main-nav`).on(`click`, `.main-nav__link`, function(evt) {
     evt.preventDefault();
     let id = $(this).attr(`href`),
         top = $(id).offset().top - 150;
+    if ($window.width() < 1150) {
+      top += 75;
+    }
     $(`.page, html`).animate({scrollTop: top}, 800);
   });
 
@@ -15,22 +20,24 @@ $(document).ready(function() {
     //Изменение фона хедера при скролле
     let position = $(this).scrollTop(),
         height = $(`.promo`).height(),
-        $nav = $(`.main-nav`),
-        $contacts = $(`.page-header__contacts`);
-    if (position > height) {
-      $nav.addClass(`main-nav--ocher`);
-      $contacts.addClass(`page-header__contacts--hidden`);
-    } else {
-      $nav.removeClass(`main-nav--ocher`);
-      $contacts.removeClass(`page-header__contacts--hidden`);
+        $contacts = $(`.page-header__contacts`),
+        $nav = $(`.main-nav`);
+    if ($window.width() > 768) {
+      if (position > height) {
+        $nav.addClass(`main-nav--ocher`);
+        $contacts.addClass(`page-header__contacts--hidden`);
+      } else {
+        $nav.removeClass(`main-nav--ocher`);
+        $contacts.removeClass(`page-header__contacts--hidden`);
+      }
     }
 
     //парслакс для блока promo
     parallaxScroll($(`.promo__wrapper`));
-  })
-});
+  });
 
-function parallaxScroll(el) {
-  let scrolled = $(window).scrollTop();
-  el.css(`top`, (0 + (scrolled * 0.5)) + `px`);
-}
+  function parallaxScroll(el) {
+    let scrolled = $window.scrollTop();
+    el.css(`top`, (0 + (scrolled * 0.5)) + `px`);
+  }
+});
