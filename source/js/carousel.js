9c3c4e4d06713e0be13fb8e866042caf52d7c75a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-30 15:01:20 +0000

    Deploy nikkov13/Center-epir to github.com/nikkov13/Center-epir.git:gh-pages

--- source/js/carousel.js
+++ source/js/carousel.js
@@ -1,39 +1,28 @@
 "use strict";
 
 $(document).ready(function() {
-  let $window = $(window),
-      $carousel = $(`.js-carousel`),
-      carouselWidth = $carousel.width(),
-      $carouselList = $(`.js-carousel__list`),
-      $carouselItems = $(`.js-carousel__item`),
-      carouselListWidth = $carouselItems.length * 260 + ($carouselItems.length - 1) * 30,
-      startX = 0,
-      carouselX;
-
-  $window.resize(function() {
-    carouselWidth = $carousel.width();
-    if ($window.width() > 768) {
-      $carouselList.css(`left`, 0);
-    } else 
-    if (parseInt($carouselList.css(`left`)) < carouselWidth - carouselListWidth) {
-      $carouselList.css(`left`, carouselWidth - carouselListWidth);
-    }
-  });
-
-  $carousel.on(`touchstart`, function(evt) {
-    let touchObj = evt.changedTouches[0];
-    startX = parseInt(touchObj.clientX);
-    carouselX = parseInt($carouselList.css(`left`));
-  });
-
-  $carousel.on(`touchmove`, function(evt) {
-    if ($window.width() <= 768) {
-      let touchObj = evt.changedTouches[0],
-          dist = (touchObj.clientX) - startX,
-          shift = carouselX + dist;
-      if (0 <= -shift && -shift <= carouselListWidth - carouselWidth) {
-        $carouselList.css(`left`, shift);
+  $(`.js-carousel__list`).slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1150,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+        }
       }
-    }
+    ]
   });
 });
