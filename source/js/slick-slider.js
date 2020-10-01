5abb79977e407b47a09d7f43092efc0c3b0006fd
Kovalev Nikolay <nikkov13@gmail.com>
2020-10-01 08:47:02 +0300

    В блоке team карточки сотрудников заменены фотогалереей, сменён заголовок и соответствующий пункт меню

--- source/js/slick-slider.js
+++ source/js/slick-slider.js
@@ -1,24 +1,21 @@
 "use strict";
 
 $(document).ready(function() {
-  $(`.js-carousel__list`).slick({
+  $(`.js-slick`).slick({
     dots: true,
     infinite: true,
-    slidesToShow: 3,
     slidesToScroll: 3,
     variableWidth: true,
     responsive: [
       {
         breakpoint: 1150,
         settings: {
-          slidesToShow: 2,
           slidesToScroll: 2,
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
           slidesToScroll: 1,
           centerMode: true,
         }
