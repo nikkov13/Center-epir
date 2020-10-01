ffa0bc90c9d3777feb2122ab5b4a36fd827572c4
Kovalev Nikolay <nikkov13@gmail.com>
2020-10-01 23:01:44 +0300

    Исправил выведение лишних точек под слайдером

--- source/js/slick-slider.js
+++ source/js/slick-slider.js
@@ -4,18 +4,21 @@ $(document).ready(function() {
   $(`.js-slick`).slick({
     dots: true,
     infinite: true,
+    slidesToShow: 3,
     slidesToScroll: 3,
     variableWidth: true,
     responsive: [
       {
         breakpoint: 1150,
         settings: {
+          slidesToShow: 2,
           slidesToScroll: 2,
         }
       },
       {
         breakpoint: 480,
         settings: {
+          slidesToShow: 1,
           slidesToScroll: 1,
           centerMode: true,
         }
